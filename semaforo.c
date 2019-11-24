954605aeb9f0b1b95283d6e90fade67742037804
Erick Pulla <erickpullads@hotmail.com>
2019-11-24 16:59:27 -0500

    Archivos de semaforos agregados

--- semaforo.c
+++ semaforo.c
@@ -1,29 +1,101 @@
 #include "semaforo.h"
-#include <getopt.h>
+#include <time.h>
 
-int main(int argc, char **argv){
-    int velocidad =20;
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
 
-    int opt;
-    while( (opt=getopt(argc, argv, "hv:")) !=-1 ){
-        switch(opt){
-            case 'h':
-                help();
-                return 1;
-            case 'v':
-                velocidad = atoi(optarg);
-        }
+#define TIEMPO_REACCION 1 // tiempo de reaccion del conductor (usualmente es 1 s)
+#define TASA_DESACELERACION 3 // tasa de desaceleracion (usualmente es 3.00 m/s2)
+#define LONGITUD_VEHICULO 6 // valor sugerido para el promedio de longitud de vehiculos (6.10 m)
+#define ANCHO_VIA 10 // ancho de la via (valor sugerido 10 m)
+
+Sem *init_sem() {
+		Sem *sem = malloc(sizeof (Sem));
+		sem->verde = 0;
+		sem->amarillo = 0;
+		sem->rojo =0;
+		return sem;
+}
+
+void setVerde(Sem *sem){
+    sem->verde = 1;
+	sem->amarillo = 0;
+	sem->rojo =0;
+}
+
+void setAmarillo(Sem *sem){
+    sem->verde = 0;
+	sem->amarillo = 1;
+	sem->rojo =0;
+}
+
+void setRojo(Sem *sem){
+    sem->verde = 0;
+	sem->amarillo = 0;
+	sem->rojo =1;
+}
+
+void liberar_sem(Sem *sem){
+    free(sem);
+}
+
+unsigned int tiempoAmarillo(unsigned int velocidad_limite){
+    return (TIEMPO_REACCION + velocidad_limite/(2*TASA_DESACELERACION));
+}
+
+unsigned int tiempoRojo(unsigned int velocidad_limite){
+    return (CICLO/2) + (LONGITUD_VEHICULO+ANCHO_VIA)/velocidad_limite - 1;
+}
+
+// unsigned int tiempoRojo(unsigned int velocidad_limite){
+//     return (CICLO/2) + (LONGITUD_VEHICULO+ANCHO_VIA)/velocidad_limite;
+// }
+
+// unsigned int tiempoVerde(unsigned int velocidad_limite){
+//     return CICLO - tiempoAmarillo(velocidad_limite) - tiempoRojo(velocidad_limite);
+// }
+
+unsigned int tiempoVerde(unsigned int velocidad_limite){
+    return CICLO - tiempoAmarillo(velocidad_limite) - tiempoRojo(velocidad_limite);
+}
+
+void empezarCiclo(Sem *sem1, Sem *sem2, unsigned int velocidad_limite){
+    unsigned int tiempo_verde = tiempoVerde(velocidad_limite);
+    unsigned int tiempo_amarillo = tiempoAmarillo(velocidad_limite);
+
+    printf("Tiempo Rojo: %d segundos\nTiempo Amarillo: %d segundos\nTiempo Verde: %d segundos\nEmpezamos el ciclo:\n",tiempo_verde + tiempo_amarillo, tiempo_amarillo, tiempo_verde);
+    while(1){
+        // Empezamos poniendo el semaforo 1 en rojo y el semaforo 2 en verde
+        setRojo(sem1);
+        setVerde(sem2);
+        imprimirVia(sem1,sem2);
+        sleep(tiempo_verde);
+
+        setAmarillo(sem2);
+        imprimirVia(sem1,sem2);
+        sleep(tiempo_amarillo);
+
+        setRojo(sem2);
+        setVerde(sem1);
+        imprimirVia(sem1,sem2);
+        sleep(tiempo_verde); 
+
+        setAmarillo(sem2);
+        imprimirVia(sem1,sem2);
+        sleep(tiempo_amarillo);
     }
+}
 
-    printf("Velocidad: %d", velocidad);
-
-    Sem *semaforo1 = init_sem();
-    Sem *semaforo2 = init_sem();
-
-    ImprimirVia(semaforo1, semaforo2);
-    setVerde(semaforo1);
-    ImprimirVia(semaforo1, semaforo2);
-
-
-    return 0;
+void tiempoEspera(unsigned int segundos){
+    // Convertimos los segundos a milisegundos
+    int ms = 1000 * 1000 * segundos; 
+  
+    // Inicializamos un tiempo de inicio
+    clock_t start_time = clock(); 
+  
+    // Iteramos hasta que el tiempo actual sea mayor o igual al tiempo requerido (inicial + milisegundos)
+    while (clock() < start_time + ms); 
 }
